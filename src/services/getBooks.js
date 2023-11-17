642fdd141e72411cb33bfb055b3abafa8887133c
Nina <frontend-developer@inbox.ru>
2023-11-17 15:38:03 +0300

    Скопировала данные с репозитория Vercel

--- src/services/getBooks.js
+++
@@ -1,8 +0,0 @@
-export const getBooks = async () => {
-  const result = await fetch('');
-  if (result.ok) {
-    console.log('При получении списка книг возникла ошибка');
-  }
-
-  return result.json();
-};
